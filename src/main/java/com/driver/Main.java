5d0182ad09b54e763502d7eeb3c873c024baba9e
Arun Bhadouriya <66020141+arun-bhadouriya@users.noreply.github.com>
2023-05-06 23:02:13 +0530

    main method implemented

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwonly = new RWOnly();
+//        rwonly.Name = "Arun";
+//        System.out.println(rwonly.Name);
+//        'Name' has private access in 'com.driver.RWOnly'
+        rwonly.setName("Arun");
+        System.out.println(rwonly.getName());
+    }
 }
